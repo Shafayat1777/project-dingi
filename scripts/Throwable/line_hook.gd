f886b30a583c60ff6b984778484395cc10ff73eb
shafayat <shafayat1777@gmail.com>
2026-08-30 22:54:45 +0600

    ci: :zap: added max range in the rope

--- scripts/Throwable/line_hook.gd
+++ scripts/Throwable/line_hook.gd
@@ -2,6 +2,7 @@ extends RigidBody2D
 
 @export var throw_speed := 800.0
 @export var recall_speed := 1000.0
+@export var max_range := 500.0
 @export var player: Node2D  # assign the character node in inspector
 
 @onready var line: Line2D = $Line2D
@@ -24,6 +25,9 @@ func _process(_delta):
 func _physics_process(_delta):
 	if is_recalling:
 		recall_hook()
+	elif is_thrown:
+		check_range()
+
 
 func _input(event):
 	if event.is_action_pressed("shoot"):
@@ -60,3 +64,7 @@ func update_line():
 	# Point 0 = player side, Point 1 = hook side
 	line.set_point_position(0, to_local(player.global_position))
 	line.set_point_position(1, Vector2(0, 14))  # hook's own origin
+
+func check_range():
+	if global_position.distance_to(player.global_position) >= max_range:
+		is_recalling = true
